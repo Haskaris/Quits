f14c6f1a62f182fbd2576021ef2ca88b9ed04c5b
Fx <fx73000@yahoo.fr>
2020-04-24 01:22:48 +0200

    Ajout historique

--- src/main/java/Modele/Historique.java
+++ src/main/java/Modele/Historique.java
@@ -1,4 +1,39 @@
 package Modele;
 
 public class Historique {
+    Commande passe;
+    Commande futur;
+
+    public void Faire(Commande c){
+        if(c == null) return ; // Correspond a une impossibilite de se deplacer pour le player. Rien n'est enregistre
+        c.Execute();
+        c.next = passe;
+        passe = c;
+        futur = null;
+    }
+
+    public void Annuler(){
+        if(PasseEstVide())return;
+        Commande c = passe.next;
+        passe.next = null;
+        futur = passe;
+        passe = c;
+        futur.Dexecute();
+    }
+
+    public void Refaire(){
+        if(FuturEstVide())return;
+        Commande c = futur.next;
+        futur.next = null;
+        passe = futur;
+        futur = c;
+        passe.Execute();
+    }
+
+    public boolean PasseEstVide(){
+        return passe == null;
+    }
+    public boolean FuturEstVide(){
+        return futur == null;
+    }
 }
