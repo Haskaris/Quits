d9a0904105ce000e467ea3900d9ad9967ba9e755
Perrier Mathis <mathis.perrier73@gmail.com>
2020-05-01 17:54:53 +0200

    Correction d'écriture → IALevel to AILevel

--- src/main/java/Global/Tools.java
+++ src/main/java/Global/Tools.java
@@ -8,7 +8,7 @@ public class Tools{
     public final static int BLANC = 2;
     public final static int NOIR = 3;
     
-    public static enum IALevel {
+    public static enum AILevel {
         Player,
         Easy,
         Medium,
