4cbcc8e66dfe5eb00b7194c9fd8531c7afc2628e
Perrier Mathis <mathis.perrier73@gmail.com>
2020-05-01 17:58:45 +0200

    Ajout de l'énumération du niveau de l'IA

--- src/main/java/Vue/EditPlayer.java
+++ src/main/java/Vue/EditPlayer.java
@@ -1,19 +1,41 @@
 package Vue;
 
+import Global.Tools.AILevel;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class EditPlayer extends JPanel {
     JTextField playerName;
-    JComboBox iaLevel;
+    JComboBox CB_aiLevel;
+    AILevel aiLevel;
     
     public EditPlayer(String playerName) {
         this.playerName = new JTextField(playerName);
         this.playerName.setPreferredSize(new Dimension(200,50));
-        this.iaLevel = new JComboBox(new Object[]{"Humain", "Ordinateur facile", "Ordinateur normal", "Ordinateur difficile"});
+        this.aiLevel = AILevel.Player;
+        this.CB_aiLevel = new JComboBox(new Object[]{"Humain", "Ordinateur facile", "Ordinateur normal", "Ordinateur difficile"});
+        this.CB_aiLevel.addActionListener ((ActionEvent e) -> {
+            switch(this.CB_aiLevel.getSelectedIndex()) {
+                case 0:
+                    aiLevel = AILevel.Player;
+                    break;
+                case 1:
+                    aiLevel = AILevel.Easy;
+                    break;
+                case 2:
+                    aiLevel = AILevel.Medium;
+                    break;
+                case 3:
+                    aiLevel = AILevel.Hard;
+                    break;
+            }
+            this.updateUI();
+        });
         this.add(this.playerName);
-        this.add(this.iaLevel);
+        this.add(this.CB_aiLevel);
+        
     }
 }
