bca166a9d824a49e3bcfeeba4a7408774e6ada8c
Perrier Mathis <mathis.perrier73@gmail.com>
2020-05-01 17:53:18 +0200

    Changement pour faciliter les tests d'IHM

--- src/main/java/Quits.java
+++ src/main/java/Quits.java
@@ -1,12 +1,14 @@
 import Global.Properties;
 import Modele.GameManager;
+import Vue.MainInterface;
 
 import java.io.IOException;
 
 public class Quits {
 
     public static void main(String[] args) throws IOException, IllegalArgumentException, IllegalAccessException {
-        Properties.Load();
-        GameManager.InstanceGame();
+        //Properties.Load();
+        //GameManager.InstanceGame();
+        new MainInterface();
     }
 }
