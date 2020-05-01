e76ab8bfe2ec860d0dc8ae198c34c2fbf18710c5
Perrier Mathis <mathis.perrier73@gmail.com>
2020-05-01 17:53:45 +0200

    Suppression du joueur dans le controleur Ajout de la calsse mediateur

--- src/main/java/Controleur/Mediateur.java
+++ src/main/java/Controleur/Mediateur.java
@@ -1,14 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Controleur;
 
-/**
- *
- * @author Mathis
- */
+import Global.Tools.IALevel;
+import Modele.GameManager;
+import Modele.Joueurs.*;
+import java.awt.Color;
+
 public class Mediateur {
+    //Voué à mourir
+    GameManager managerDeJeu;
     
+    public Mediateur() {
+        managerDeJeu = new GameManager();
+    }
+    
+    private Joueur newPlayerHuman(String playerName, Color color) {
+        return new JoueurHumain(playerName, color);
+    }
+    
+    private Joueur newPlayerAIEeasy(String playerName, Color color) {
+        return new JoueurIAFacile(playerName, color);
+    }
+    
+    private Joueur newPlayerAIMedium(String playerName, Color color) {
+        return new JoueurIANormale(playerName, color);
+    }
+    
+    private Joueur newPlayerAIHard(String playerName, Color color) {
+        return new JoueurIADifficile(playerName, color);
+    }
+    
+    public void addPlayer(String playerName, Color color, IALevel level) {
+        switch(level) {
+            case Player:
+                this.managerDeJeu.addPlayer(newPlayerHuman(playerName, color));
+                break;
+            case Easy:
+                this.managerDeJeu.addPlayer(newPlayerAIEeasy(playerName, color));
+                break;
+            case Hard:
+                this.managerDeJeu.addPlayer(newPlayerAIHard(playerName, color));
+                break;
+            case Medium:
+                this.managerDeJeu.addPlayer(newPlayerAIMedium(playerName, color));
+                break;
+        }
+    }
+    
+    public Joueur getPlayer(int index) {
+        return this.managerDeJeu.joueurs.get(index);
+    }
 }
