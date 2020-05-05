dfe0f461d29c2189b45f1e32237c0f70df91f05c
Perrier Mathis <mathis.perrier73@gmail.com>
2020-05-05 18:00:28 +0200

    Ajout des paterns

---
+++ src/main/java/Paterns/Observable.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Paterns;
+
+/*
+ * Pattern Observateur tel que présenté dans le livre de Gamma et Al.
+ * Ce pattern existe déjà dans la bibliothèque standard de Java sous une forme
+ * légèrement différente. Il est réimplémenté ici à des fins pédagogiques
+ */
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Observable {
+	List<Observateur> observateurs;
+
+	public Observable() {
+		observateurs = new ArrayList<>();
+	}
+
+	public void ajouteObservateur(Observateur o) {
+		observateurs.add(o);
+	}
+
+	public void metAJour() {
+		Iterator<Observateur> it;
+
+		it = observateurs.iterator();
+		while (it.hasNext()) {
+			Observateur o = it.next();
+			o.miseAJour();
+		}
+	}
+}
+
---
+++ src/main/java/Paterns/Observateur.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Paterns;
+
+/*
+ * Pattern Observateur tel que présenté dans le livre de Gamma et Al.
+ * Ce pattern existe déjà dans la bibliothèque standard de Java sous une forme
+ * légèrement différente. Il est réimplémenté ici à des fins pédagogiques
+ */
+public interface Observateur {
+	void miseAJour();
+}
